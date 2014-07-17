6ec1cda67f76e9a023377007f842bdadb036b994
sonots <sonots@gmail.com>
2014-07-17 12:37:19 +0900

    fix not to apply resolver replace just by `require`

--- example/example.rb
+++ example/example.rb
@@ -1,11 +1,16 @@
-require 'resolve'
+require 'resolv'
+require 'resolver_replace'
 
 resolver = Resolv::DNS.new(:nameserver => ['210.251.121.21'],
                            :search => ['ruby-lang.org'],
                            :ndots => 1)
 
-ResolverReplace.configure(
-  getaddress_proc: Proc.new {|host| resolver.getaddress(host) },
-  getaddresses_proc: Proc.new {|host| resolver.getaddresses(host) },
+addr = IPSocket.getaddress('ruby-lang.org') # Should not use ResolverReplace yet
+
+ResolverReplace.register!(
+  getaddress: Proc.new {|host| resolver.getaddress(host) },
+  getaddresses: Proc.new {|host| resolver.getaddresses(host) },
   error_class: Resolv::ResolvError,
 )
+
+addr = IPSocket.getaddress('ruby-lang.org') # Should use ResolverReplace
--- lib/resolver_replace.rb
+++ lib/resolver_replace.rb
@@ -9,6 +9,7 @@ class ResolverReplace
       raise ArgumentError, 'getaddresses is required'
     end
     @error_class = params[:error_class] || StandardError
+    require 'resolver_replace/replace.rb'
   end
 
   def self.getaddress(host)
@@ -28,77 +29,3 @@ class ResolverReplace
   end
 end
 
-# reference: ruby/lib/resolv-replace.rb
-require 'socket'
-
-class << IPSocket
-  # :stopdoc:
-  alias original_resolver_getaddress getaddress
-  # :startdoc:
-  def getaddress(host)
-    begin
-      return ResolverReplace.getaddress(host).to_s
-    rescue ResolverReplace.error_class => e
-      raise SocketError, "#{e.class} #{e.message}"
-    end
-  end
-end
- 
-class TCPSocket < IPSocket
-  # :stopdoc:
-  alias original_resolver_initialize initialize
-  # :startdoc:
-  def initialize(host, serv, *rest)
-    rest[0] = IPSocket.getaddress(rest[0]) unless rest.empty?
-    original_resolver_initialize(IPSocket.getaddress(host), serv, *rest)
-  end
-end
- 
-class UDPSocket < IPSocket
-  # :stopdoc:
-  alias original_resolver_bind bind
-  # :startdoc:
-  def bind(host, port)
-    host = IPSocket.getaddress(host) if host != ""
-    original_resolver_bind(host, port)
-  end
- 
-  # :stopdoc:
-  alias original_resolver_connect connect
-  # :startdoc:
-  def connect(host, port)
-    original_resolver_connect(IPSocket.getaddress(host), port)
-  end
- 
-  # :stopdoc:
-  alias original_resolver_send send
-  # :startdoc:
-  def send(mesg, flags, *rest)
-    if rest.length == 2
-      host, port = rest
-      begin
-        addrs = ResolverReplace.getaddresses(host)
-      rescue ResolverReplace.error_class => e
-        raise SocketError, "#{e.class} #{e.message}"
-      end
-      addrs[0...-1].each {|addr|
-        begin
-          return original_resolver_send(mesg, flags, addr, port)
-        rescue SystemCallError
-        end
-      }
-      original_resolver_send(mesg, flags, addrs[-1], port)
-    else
-      original_resolver_send(mesg, flags, *rest)
-    end
-  end
-end
- 
-class SOCKSSocket < TCPSocket
-  # :stopdoc:
-  alias original_resolver_initialize initialize
-  # :startdoc:
-  def initialize(host, serv)
-    original_resolver_initialize(IPSocket.getaddress(host), port)
-  end
-end if defined? SOCKSSocket
